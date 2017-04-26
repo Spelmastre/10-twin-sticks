52e9a7bf9f311a1e37c47088557ef5a2434eec9d
Tim Calabro <tim@epicappsolutions.com>
2017-04-25 23:25:32 -0400

    Finished setting up struct for MyKeyFrame

---
+++ Assets/Scripts/MyReplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public struct MyKeyFrame 
+{
+    public float frameTime;
+    public Vector3 framePos;
+    public Quaternion frameRotation;
+
+    public MyKeyFrame (float time, Vector3 pos, Quaternion rot)
+    {
+        this.frameTime = time;
+        this.framePos = pos;
+        this.frameRotation = rot;
+    }
+}
